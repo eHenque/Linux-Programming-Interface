b217fb1074a0e84ba6dff6aeb60836f8540e1af1
Hector LAOS <he_laos@hotmail.com>
2018-12-23 12:38:04 +0100

    Sockets chapter 57

---
+++ Socket_Server/us_xfr.h
@@ -0,0 +1,20 @@
+/*
+ * us_xfr.h
+ *
+ *  Created on: Nov 10, 2018
+ *      Author: hector
+ */
+
+#ifndef US_XFR_H_
+#define US_XFR_H_
+
+
+#include <sys/un.h>
+#include <sys/socket.h>
+#include "lib/tlpi_hdr.h"
+
+#define SV_SOCK_PATH "/tmp/us_xfr"
+#define BUF_SIZE 100
+
+
+#endif /* US_XFR_H_ */
